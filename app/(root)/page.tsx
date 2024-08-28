0d599c8673f4ec4f029b15e5e8566e5ef59830a1
Yuan <yuankomah211@gmail.com>
2024-08-28 22:48:37 +1000

    finish most of the layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
+  const user = { firstName: 'Yuan', lastName: 'Komah', email: 'yuankomah211@gmail.com' };
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user="Guess"
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={10000.35}
+          />
+        </header>
+      </div>
+      <RightSidebar 
+        user={user}
+        transactions={[]}
+        banks={[{}, {}]}/>
+    </section>
   )
 }
 
